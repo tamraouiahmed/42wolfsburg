0f4421a308a344e05db376cb3fa3b23f02eb20f0
chiki <test@live.fr>
2022-03-05 00:38:13 +0100

    h,gh,g

---
+++ banana/chadi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int attay (int a,int b,int c)
+{
+    if ((a>b) && (c=10))
+    printf("A ");
+    else if ((b>a) && (c=50))
+    {
+    printf("B/",50);
+    }
+    else 
+     printf("aallah ghaleb");
+}
+int main()
+{int c;
+    attay(15,15,c);
+}
