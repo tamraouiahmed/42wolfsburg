66099b8112412a9627ff178a5711f0e735f12228
chiki <test@live.fr>
2022-03-05 16:20:28 +0100

    cra

---
+++ ben.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int chakka(int a)
+{  
+    if (a<0)
+    {
+        printf("n\n");
+    }
+
+else if (a==0)
+ {
+     printf("null\n");
+ }
+ else 
+ {
+     printf("p");
+ }
+}
