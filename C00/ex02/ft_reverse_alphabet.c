8201ee0c8c16fdaf160b8ff7604abb544c5d8182
chiki <test@live.fr>
2022-03-06 23:57:23 +0100

    bay

---
+++ C00/ex02/ft_reverse_alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+int main()
+{
+    
+  int i=122;
+  while(i>=97)
+  {
+      write(1,&i,1);
+      i--;
+
+    
+
+      
+  }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
